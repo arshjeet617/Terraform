0fb3a4f6e9ce7e738aa83c0be94820a7a82ee062
arshjeet617 <103775681+arshjeet617@users.noreply.github.com>
2022-06-03 01:58:36 -0400

    Add files via upload

---
+++ vpc.tf
@@ -0,0 +1,52 @@
+# Step 1 - Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  public_cidrs = "10.15.0.0/24"
+  vpc_cidr     = "10.15.0.0/16"
+}
+
+# Create a new VPC 
+resource "aws_vpc" "main" {
+  cidr_block       = local.vpc_cidr
+  instance_tenancy = "default"
+}
+
+
+
+# Add provisioning of the public subnet2 in the custom VPC
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = local.public_cidrs
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route table to route add default gateway pointing to Internet Gateway (IGW)
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
+
+
+# Associate subnets with the custom route table
+resource "aws_route_table_association" "public_route_table_association-2" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_2.id
+}
